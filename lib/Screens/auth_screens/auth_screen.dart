878457c0f7330224af1be796b0c806f77eaef899
salma mohamed <salma.fayad203@gmail.com>
2021-03-24 18:16:58 +0200

    start auth firebase

--- lib/Screens/auth_screens/auth_screen.dart
+++ lib/Screens/auth_screens/auth_screen.dart
@@ -1,11 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:solution_challenge_project/Widgets/auth_widgets/Auth_form.dart';
+
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  void _submitAuthForm(String email, String password, String username,
+      bool isLogin, BuildContext ctx) async {
+    UserCredential authResult;
+    try {
+      if (isLogin) {
+        ///login
+        authResult= await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        ///sign up
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      else if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
