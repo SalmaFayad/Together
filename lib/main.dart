63f6caed9be16819ae09fdf6d8a27a281f4d302c
Amrmahmoud333 <3mrma7moud69@gmail.com>
2021-03-25 19:45:07 +0200

    Adding comment

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Screens/auth_screens/auth_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); //init firebase
   runApp(MyApp());
 }
 
@@ -12,21 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-        scaffoldBackgroundColor: Colors.blue,
-        primarySwatch: Colors.blue,
-        accentColor: Colors.blueAccent,
-        buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor: Colors.blue,
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+        theme: ThemeData(
+          primaryColor: Colors.blue,
+          scaffoldBackgroundColor: Colors.blue,
+          primarySwatch: Colors.blue,
+          accentColor: Colors.blueAccent,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+            buttonColor: Colors.blue,
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
           ),
         ),
-      ),
-      home: AuthScreen()
-    );
+        home: AuthScreen());
   }
 }
-
