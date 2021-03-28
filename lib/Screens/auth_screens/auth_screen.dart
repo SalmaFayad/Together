d645f175c19d6ee424dd8824e3724810354690d9
waelahmed99 <waelahme6599@gmail.com>
2021-03-28 19:25:27 +0200

    Handling no password

--- lib/Screens/auth_screens/auth_screen.dart
+++ lib/Screens/auth_screens/auth_screen.dart
@@ -7,23 +7,22 @@ import 'package:solution_challenge_project/Widgets/auth_widgets/Auth_form.dart';
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
-
 }
 
 class _AuthScreenState extends State<AuthScreen> {
   final _auth = FirebaseAuth.instance;
 
-  bool _isLoading =false;
+  bool _isLoading = false;
   void _submitAuthForm(String email, String password, String username,
       bool isLogin, BuildContext ctx) async {
     UserCredential authResult;
     try {
       setState(() {
-        _isLoading=true;
+        _isLoading = true;
       });
       if (isLogin) {
         ///login
-        authResult= await _auth.signInWithEmailAndPassword(
+        authResult = await _auth.signInWithEmailAndPassword(
           email: email,
           password: password,
         );
@@ -34,34 +33,36 @@ class _AuthScreenState extends State<AuthScreen> {
           password: password,
         );
         await FirebaseFirestore.instance
-        .collection('users').doc(authResult.user.uid)
-        .set({
-          'username':username,
-          'password':password,
+            .collection('users')
+            .doc(authResult.user.uid)
+            .set({
+          'username': username,
+          'password': password,
         });
       }
     } on FirebaseAuthException catch (e) {
-      String message ='Error occurred';
+      String message = 'Error occurred';
+      if (password.isEmpty) message = 'Please enter your password';
       if (e.code == 'weak-password') {
-        message='The password provided is too weak.';
+        message = 'The password provided is too weak.';
       } else if (e.code == 'email-already-in-use') {
-        message='The account already exists for that email.';
-      }
-      else if (e.code == 'user-not-found') {
-        message='No user found for that email.';
+        message = 'The account already exists for that email.';
+      } else if (e.code == 'user-not-found') {
+        message = 'No user found for that email.';
       } else if (e.code == 'wrong-password') {
-        message='Wrong password provided for that user.';
+        message = 'Wrong password provided for that user.';
       }
       Scaffold.of(ctx).showSnackBar(SnackBar(
         content: Text(message),
-        backgroundColor: Theme.of(ctx).errorColor,));
+        backgroundColor: Theme.of(ctx).errorColor,
+      ));
       setState(() {
-        _isLoading=false;
+        _isLoading = false;
       });
     } catch (e) {
       print(e);
       setState(() {
-        _isLoading=false;
+        _isLoading = false;
       });
     }
   }
@@ -69,7 +70,7 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthForm(_submitAuthForm ,_isLoading),
+      body: AuthForm(_submitAuthForm, _isLoading),
     );
   }
 }
