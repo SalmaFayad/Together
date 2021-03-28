746d08eef0836626fb2a35c3ee3e4cddbd88fe49
waelahmed99 <waelahme6599@gmail.com>
2021-03-28 21:01:58 +0200

    Creating user model class

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String name;
+  String email;
+  String city;
+  String phone;
+  String country;
+
+  User({
+    this.name,
+    this.email,
+    this.city,
+    this.country,
+    this.phone,
+  });
+}
